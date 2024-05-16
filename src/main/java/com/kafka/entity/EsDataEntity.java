2f973ecffdb4366910cb85c9724a2542ac2944dc
DESKTOP-PIB4DJF\lisheng <524892125@QQ.COM>
2024-05-16 16:44:27 +0800

    kafka

---
+++ src/main/java/com/kafka/entity/EsDataEntity.java
@@ -0,0 +1,15 @@
+package com.kafka.entity;
+
+public class EsDataEntity {
+    public String name;
+
+    public String content;
+
+    public String url;
+
+    public String detail_id;
+
+    public String recorder_id;
+
+    public int user_id;
+}
