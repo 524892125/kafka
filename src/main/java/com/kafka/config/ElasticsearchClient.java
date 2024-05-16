2d21a5f3b3214f54501ae9def58e79e75023aac1
DESKTOP-PIB4DJF\lisheng <524892125@QQ.COM>
2024-05-16 17:12:57 +0800

    kafka

--- src/main/java/com/kafka/config/ElasticsearchClient.java
+++ src/main/java/com/kafka/config/ElasticsearchClient.java
@@ -1,6 +1,10 @@
 package com.kafka.config;
 
 import jakarta.annotation.PostConstruct;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.RestClient;
 import org.apache.http.HttpHost;
@@ -24,10 +28,19 @@ public class ElasticsearchClient {
 
     @PostConstruct
     public void run() {
+        CredentialsProvider credentialsProvider;
+        if (false) { // 如果有密码
+            credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elastic", "zhihuigw-elasticsearch"));
+        } else {
+            credentialsProvider = null;
+        }
+
+
         client = new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost(host, port, scheme)
-                )
+                ).setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
         );
     }
 
